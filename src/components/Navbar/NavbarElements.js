e01713a938dbf8da1a0831105685823d5dee8cfb
kimjaei00 <jgim0417@gmail.com>
2022-06-28 22:37:21 +0900

    bar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 import {Link as LinkR} from 'react-router-dom'
-//import {Link as LinkS} from 'react-scroll'
+import {Link as LinkS} from 'react-scroll'
 
 
 export const Nav = styled.nav`
  background : #000;
  height: 80px;
- margin-top:-80px;
+
  display: flex;
  justify-content: center;
  align-items: center;
@@ -19,7 +19,7 @@ z-index :10;
     transition : 0.8s all ease;
 }
 
-`
+`;
 export const NavbarContainer = styled.div`
  display: flex;
  justify-content: space-between;
@@ -30,9 +30,9 @@ export const NavbarContainer = styled.div`
  max-width: 1100px;
 
 
-`
+`;
 export const NavLogo =styled(LinkR)`
- color:#fff;
+ color:red;
  justify-self : flex-start;
  cursor: pointer;
  font-size : 1.5rem;
@@ -41,4 +41,48 @@ export const NavLogo =styled(LinkR)`
  margin-left: 24px;
  font-weight:bold;
  text-decoration: none;
-`
+`;
+
+export const MobileIcon = styled.div`
+    display : none;
+@media screen and (max-width: 768px){
+    display: block;
+    position: absolute;
+    top : 0;
+    right:0;
+    transform:translate(-100%, 60%);
+    font-size:1.8rem;
+    cursor:pointer;
+    color: #fff;
+ }
+`;
+
+export const NavMenu = styled.ul`
+ display: flex;
+ align-items:center;
+ list-style:none;
+ text-align:center;
+ margin-right:-22px;
+
+ @media screen and (max-width:768px){
+    display :none;
+ }
+`;
+
+export const NavItem = styled.li`
+ height : 80px;
+
+`;
+
+export const NavLinks=styled(LinkS)`
+ color:#fff
+ display: flex;
+ align-items:center;
+ text-decoration: none;
+ padding: 0 1 rem;
+ height: 100%;
+ cursor: pointer;
+ &.active{
+    border-bottom:3px solid #01bf71;
+ }
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import{Nav,NavbarContainer,NavLogo} from './NavbarElements';
+import { FaBars } from 'react-icons/fa'
+import{Nav,NavbarContainer,NavLogo,MobileIcon,NavMenu,NavItem,NavLinks} from './NavbarElements';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-            <NavLogo>freemed</NavLogo>
+            <NavLogo to="/">freemed</NavLogo>
+            <MobileIcon>
+                <FaBars />
+            </MobileIcon>
+            <NavMenu>
+                <NavItem>
+                    <NavLinks to ="about">About</NavLinks>
+                </NavItem>
+            </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
